e24bdc1d7686a7a9c591568095c5b3fa73213f2c
MahmoudMorsyDev <mike.the.dean.1002@gmail.com>
2023-07-25 10:33:55 +0200

    test upload profile picture

--- src/test/kotlin/com/belle_musica/AlbumsPageTest.kt
+++ src/test/kotlin/com/belle_musica/AlbumsPageTest.kt
@@ -1,101 +1,98 @@
 package com.belle_musica
+import com.belleMusica.handlers.dotenv
 import com.belleMusica.schemas.Users
 import database
-import okhttp3.OkHttpClient
-import org.http4k.client.OkHttp
 import org.http4k.core.*
-import org.http4k.core.cookie.cookie
-import org.http4k.lens.WebForm
-import org.junit.jupiter.api.AfterAll
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
-import org.ktorm.dsl.delete
-import org.ktorm.dsl.eq
 import org.openqa.selenium.WebDriver
-import org.riversun.okhttp3.OkHttp3CookieHelper
 import org.openqa.selenium.By
 import org.openqa.selenium.chrome.ChromeDriver
 import org.junit.jupiter.api.TestInstance
 import org.ktorm.dsl.deleteAll
-import org.ktorm.entity.filter
-import org.ktorm.entity.firstOrNull
-import org.ktorm.entity.sequenceOf
 import org.openqa.selenium.chrome.ChromeOptions
-import sessionCache
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
 class AlbumsPageTest {
 
     private lateinit var driver: WebDriver
 
-    @BeforeAll
-    fun setup() {
+
+    @Test
+    fun `Test like album`() {
+        setupAutoUser()
+        likeAnAlbum()
+        val albumElements = driver.findElements(By.className("album"))
+        val firstElement = albumElements[0].text
+        assert(firstElement.contains(">Music"))
+        driverShutDown()
+    }
+
+
+    @Test
+    fun `Test user's liked album appears on profile page`() {
+        setupAutoUser()
+        likeAnAlbum()
+        val clickOnProfileButton = driver.findElement(By.id("profile_picture")).click()
+        Thread.sleep(1000)
+        val likedAlbumElements = driver.findElements(By.className("album"))
+        val firstElement = likedAlbumElements[0].text
+        assert(firstElement.contains(">Music"))
+        Thread.sleep(1000)
+        driverShutDown()
+    }
+    @Test
+    fun `Test upload profile picture and check if displayed`() {
+        setupAutoUser()
+        val clickOnProfileButton = driver.findElement(By.id("profile_picture")).click()
+        val fileInput = driver.findElement(By.id("profilePictureInput"))
+        val filePath = dotenv["TEST_PHOTO"]
+        fileInput.sendKeys(filePath)
+        driver.findElement(By.id("photoForm")).submit()
+        val uploadedImage = driver.findElement(By.id("profilePicture"))
+        assert(uploadedImage.isDisplayed)
+    }
+
+    @Test
+    fun `Test logout and check rendering the most popular albums`() {
+        setupAutoUser()
+        likeAnAlbum()
+        val logoutButton = driver.findElement(By.id("signout")).click()
+        val albumElements = driver.findElements(By.className("likedAlbumCard"))
+        val firstElement = albumElements[0].text
+        assert(firstElement.contains(">Music"))
+        driverShutDown()
+    }
+
+
+
+
+
+    fun likeAnAlbum(){
+        val likeButton = driver.findElement(By.id("likeButton>Music")).click()
+        Thread.sleep(1000)
+    }
+
+
+
+    fun setupAutoUser() {
         database.deleteAll(Users)
         val chromeOptions = ChromeOptions()
         chromeOptions.addArguments("--headless")
         chromeOptions.addArguments("--disable-gpu")
-        driver = ChromeDriver()
+        driver = ChromeDriver(chromeOptions)
         driver.get("http://localhost:9999/")
         val registerButton = driver.findElement(By.id("register-btn")).click()
-        Thread.sleep(2000)
         val email: Unit = driver.findElement(By.id("emailinput")).sendKeys("mail@mail2.com")
-        Thread.sleep(2000)
         val password: Unit = driver.findElement(By.id("passwordinput")).sendKeys("Password@123")
-        Thread.sleep(2000)
-        val username: Unit = driver.findElement(By.id("usernameinput")).sendKeys("Ahmed")
-        Thread.sleep(2000)
+        val username: Unit = driver.findElement(By.id("usernameinput")).sendKeys("Sara")
         val submit = driver.findElement(By.id("submitbutton")).click()
-        Thread.sleep(2000)
-
-        val createdUser = database.sequenceOf(Users)
-            .filter {it.email eq "mail@mail2.com"}
-            .firstOrNull()
-        assert(createdUser!= null)
         val emailLogin: Unit = driver.findElement(By.id("email")).sendKeys("mail@mail2.com")
-        Thread.sleep(2000)
         val passwordLogin: Unit = driver.findElement(By.id("password")).sendKeys("Password@123")
-        Thread.sleep(2000)
         val submitLogin = driver.findElement(By.id("submit")).click()
-        Thread.sleep(2000)
     }
 
-//   @AfterAll
-//   fun driverShutDown(){
-//       driver.quit()
-//   }
-
-    @Test
-    fun `like an album and test if it goes to the first place`() {
-        val likeButton = driver.findElement(By.id("likeButton>Music")).click()
-        Thread.sleep(2000)
-        val albumElements = driver.findElements(By.className("album"))
-        val firstElement = albumElements[0].text
-        assert(firstElement.contains(">Music"))
-        Thread.sleep(2000)
+    fun driverShutDown(){
+        driver.quit()
     }
-    @Test
-    fun `test if the liked album appears in the user profile page`() {
-        val clickOnProfileButton = driver.findElement(By.id("profile_picture")).click()
-        Thread.sleep(2000)
-        val likedAlbumElements = driver.findElements(By.className("album"))
-        val firstElement = likedAlbumElements[0].text
-        assert(firstElement.contains(">Music"))
-        Thread.sleep(2000)
-    }
-
-    @AfterAll
-    @Test
-    fun `test logout the user and check going back to the main page`() {
-        val logoutButton = driver.findElement(By.id("signout")).click()
-        Thread.sleep(2000)
-        val albumElements = driver.findElements(By.className("likedAlbumCard"))
-        val firstElement = albumElements[0].text
-        assert(firstElement.contains(">Music"))
-    }
-
-
-
 }
--- src/test/kotlin/com/belle_musica/RegisterUserTest.kt
+++ src/test/kotlin/com/belle_musica/RegisterUserTest.kt
@@ -3,11 +3,10 @@ package com.belle_musica
 import com.belleMusica.schemas.Users
 import database
 import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
-import org.ktorm.dsl.delete
+import org.ktorm.dsl.deleteAll
 import org.ktorm.dsl.eq
 import org.ktorm.entity.*
 import org.openqa.selenium.By
@@ -20,41 +19,30 @@ class RegisterUserTest {
 
     private lateinit var driver: WebDriver
 
-    companion object {
-        @JvmStatic
-        @BeforeAll
-        fun restoreDb(): Unit {
-            database.delete(Users) { it.id eq it.id }
-        }
-    }
-    @BeforeEach
-    fun driver(){
-//        val chromeOptions = ChromeOptions()
-//        chromeOptions.addArguments("--headless")
-//        chromeOptions.addArguments("--disable-gpu")
-        driver = ChromeDriver()
-        driver.get("http://localhost:9999/users/new")
-    }
+
     @AfterEach
     fun closeBrowser(){
         driver.quit()
     }
+    @BeforeEach
+    fun resetDb(){
+        database.deleteAll(Users)
+        val chromeOptions = ChromeOptions()
+        chromeOptions.addArguments("--headless")
+        chromeOptions.addArguments("--disable-gpu")
+        driver = ChromeDriver(chromeOptions)
+        driver.get("http://localhost:9999/users/new")
+    }
 
     @Test
     fun `Successfully created a new user`() {
-        val email: Unit = driver.findElement(By.id("emailinput")).sendKeys("mail@mail2.com")
-        Thread.sleep(2000)
-        val password: Unit = driver.findElement(By.id("passwordinput")).sendKeys("Password@123")
-        Thread.sleep(2000)
-        val username: Unit = driver.findElement(By.id("usernameinput")).sendKeys("Ahmed")
-        Thread.sleep(2000)
-        val submit = driver.findElement(By.id("submitbutton")).click()
-        Thread.sleep(2000)
+
+        setupUser()
         val ourUser = database.sequenceOf(Users)
             .filter { it.email eq "mail@mail2.com" }
             .firstOrNull()
         assert(ourUser != null)
-        println("AQUIIIII" + ourUser)
+
 
     }
 
@@ -64,26 +52,31 @@ class RegisterUserTest {
         Thread.sleep(2000)
         val password: Unit = driver.findElement(By.id("passwordinput")).sendKeys("Password@123")
         Thread.sleep(2000)
-        val username: Unit = driver.findElement(By.id("usernameinput")).sendKeys("Ahmed")
+        val username: Unit = driver.findElement(By.id("usernameinput")).sendKeys("Sara")
         Thread.sleep(2000)
         val submit = driver.findElement(By.id("submitbutton")).click()
         Thread.sleep(2000)
-        val ourUser = database.sequenceOf(Users).toList().size
-        assert(ourUser == 1)
+        val resultPageText = driver.findElement(By.tagName("body")).text
+        assert(resultPageText.contains("The email is incorrect, choose a valid email."))
     }
 
     @Test
     fun `User Not created because of existing email`() {
+        setupUser()
+        val registerButton = driver.findElement(By.id("register-btn")).click()
+
         val email: Unit = driver.findElement(By.id("emailinput")).sendKeys("mail@mail2.com")
         Thread.sleep(2000)
         val password: Unit = driver.findElement(By.id("passwordinput")).sendKeys("Password@123")
         Thread.sleep(2000)
-        val username: Unit = driver.findElement(By.id("usernameinput")).sendKeys("Ahmed2")
+        val username: Unit = driver.findElement(By.id("usernameinput")).sendKeys("Sara")
         Thread.sleep(2000)
         val submit = driver.findElement(By.id("submitbutton")).click()
         Thread.sleep(2000)
         val ourUser = database.sequenceOf(Users).toList().size
         assert(ourUser == 1)
+        val resultPageText = driver.findElement(By.tagName("body")).text
+        assert(resultPageText.contains("The user already exists, choose another email."))
     }
 
     @Test
@@ -92,26 +85,35 @@ class RegisterUserTest {
         Thread.sleep(2000)
         val password: Unit = driver.findElement(By.id("passwordinput")).sendKeys("123")
         Thread.sleep(2000)
-        val username: Unit = driver.findElement(By.id("usernameinput")).sendKeys("Ahmed")
+        val username: Unit = driver.findElement(By.id("usernameinput")).sendKeys("Sara")
         Thread.sleep(2000)
         val submit = driver.findElement(By.id("submitbutton")).click()
         Thread.sleep(2000)
         val ourUser = database.sequenceOf(Users).toList().size
-        assert(ourUser == 1)
+        assert(ourUser == 0)
+        val resultPageText = driver.findElement(By.tagName("body")).text
+        assert(resultPageText.contains("The password should contain at least one digit, one lowercase letter, one uppercase letter, one special character and have a minimum length of 8 characters."))
     }
 
     @Test
     fun `User Not created because of existing username`() {
-        val email: Unit = driver.findElement(By.id("emailinput")).sendKeys("mail1@mail2.com")
-        Thread.sleep(2000)
-        val password: Unit = driver.findElement(By.id("passwordinput")).sendKeys("Password@123")
-        Thread.sleep(2000)
-        val username: Unit = driver.findElement(By.id("usernameinput")).sendKeys("Ahmed")
-        Thread.sleep(2000)
-        val submit = driver.findElement(By.id("submitbutton")).click()
-        Thread.sleep(2000)
-        val ourUser = database.sequenceOf(Users).toList().size
+        setupUser()
+        val registerButton = driver.findElement(By.id("register-btn")).click()
 
-        assert(ourUser == 1)
+        val email: Unit = driver.findElement(By.id("emailinput")).sendKeys("mail1@mail2.com")
+        val password: Unit = driver.findElement(By.id("passwordinput")).sendKeys("Password@123")
+        val username: Unit = driver.findElement(By.id("usernameinput")).sendKeys("Sara")
+        val submit = driver.findElement(By.id("submitbutton")).click()
+        val resultPageText = driver.findElement(By.tagName("body")).text
+        assert(resultPageText.contains("The user already exists, choose another username."))
+
     }
+    fun setupUser(){
+            val registerButton = driver.findElement(By.id("register-btn")).click()
+            val email: Unit = driver.findElement(By.id("emailinput")).sendKeys("mail@mail2.com")
+            val password: Unit = driver.findElement(By.id("passwordinput")).sendKeys("Password@123")
+            val username: Unit = driver.findElement(By.id("usernameinput")).sendKeys("Sara")
+            val submit = driver.findElement(By.id("submitbutton")).click()
+    }
+
 }
