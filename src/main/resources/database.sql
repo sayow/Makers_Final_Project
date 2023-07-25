b3b29bc896b10c7b2bbe12217163fbecd4777133
vanessafcosta <bio.vfc@gmail.com>
2023-07-25 11:00:59 +0100

    Fixed hbs for profile pic and submmit btn, and fix for the db (frop followers and likes)

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -6,6 +6,8 @@
 -- Database: belle_musica
 -- Generation Time: 2023-04-25 11:31:23.3270
 -- -------------------------------------------------------------
+DROP TABLE IF EXISTS "public"."followers" CASCADE;
+DROP TABLE IF EXISTS "public"."likes" CASCADE;
 DROP TABLE IF EXISTS "public"."users" CASCADE;
 -- This script only contains the table creation statements and does not fully represent the table in the database. It's still missing: indices, triggers. Do not use it as a backup.
 -- Sequence and defined type
