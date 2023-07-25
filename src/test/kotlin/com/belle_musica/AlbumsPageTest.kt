6d501b2dafed9945abcb0dbb5c2599ba1017b889
MahmoudMorsyDev <mike.the.dean.1002@gmail.com>
2023-07-25 10:56:36 +0200

    all tests wrok

--- src/test/kotlin/com/belle_musica/AlbumsPageTest.kt
+++ src/test/kotlin/com/belle_musica/AlbumsPageTest.kt
@@ -1,91 +1,137 @@
 package com.belle_musica
+import com.belleMusica.handlers.dotenv
 import com.belleMusica.schemas.Users
 import database
-import okhttp3.OkHttpClient
-import org.http4k.client.OkHttp
 import org.http4k.core.*
-import org.http4k.lens.WebForm
-import org.junit.jupiter.api.AfterAll
-import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.ktorm.dsl.delete
-import org.ktorm.dsl.eq
 import org.openqa.selenium.WebDriver
-import org.riversun.okhttp3.OkHttp3CookieHelper
 import org.openqa.selenium.By
 import org.openqa.selenium.chrome.ChromeDriver
 import org.junit.jupiter.api.TestInstance
+import org.ktorm.dsl.deleteAll
 import org.openqa.selenium.chrome.ChromeOptions
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+
+
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
 class AlbumsPageTest {
-    private val cookieHelper = OkHttp3CookieHelper()
-    val client = OkHttp(OkHttpClient().newBuilder().cookieJar(cookieHelper.cookieJar()).build())
-    val signUpForm = WebForm(
-        mapOf(
-            "email" to listOf("email@email.com"),
-            "password" to listOf("Password@123"),
-            "username" to listOf("user")
-        )
-    )
-    val loginForm = WebForm(
-        mapOf(
-            "email" to listOf("email@email.com"),
-            "password" to listOf("Password@123"),
-        )
-    )
+
     private lateinit var driver: WebDriver
 
-    @BeforeAll
-    fun silentMood() {
+
+    @Test
+    fun `Test like album`() {
+        setupAutoUser()
+        likeAnAlbum()
+        val albumElements = driver.findElements(By.className("album"))
+        val firstElement = albumElements[0].text
+        assert(firstElement.contains(">Music"))
+        driverShutDown()
+    }
+
+
+    @Test
+    fun `Test user's liked album appears on profile page`() {
+        setupAutoUser()
+        likeAnAlbum()
+        val clickOnProfileButton = driver.findElement(By.id("profile_picture")).click()
+        Thread.sleep(1000)
+        val likedAlbumElements = driver.findElements(By.className("album"))
+        val firstElement = likedAlbumElements[0].text
+        assert(firstElement.contains(">Music"))
+        Thread.sleep(1000)
+        driverShutDown()
+    }
+    @Test
+    fun `Test upload profile picture and check if displayed`() {
+        setupAutoUser()
+        val clickOnProfileButton = driver.findElement(By.id("profile_picture")).click()
+        val fileInput = driver.findElement(By.id("profilePictureInput"))
+        val filePath = dotenv["TEST_PHOTO"]
+        fileInput.sendKeys(filePath)
+        driver.findElement(By.id("photoForm")).submit()
+        val uploadedImage = driver.findElement(By.id("profilePicture"))
+        assert(uploadedImage.isDisplayed)
+    }
+
+    @Test
+    fun `Test logout and check rendering the most popular albums`() {
+        setupAutoUser()
+        likeAnAlbum()
+        val logoutButton = driver.findElement(By.id("signout")).click()
+        val albumElements = driver.findElements(By.className("likedAlbumCard"))
+        val firstElement = albumElements[0].text
+        assert(firstElement.contains(">Music"))
+        driverShutDown()
+    }
+
+
+
+
+
+    fun likeAnAlbum(){
+        val likeButton = driver.findElement(By.id("likeButton>Music")).click()
+        Thread.sleep(1000)
+    }
+
+    @Test
+    fun testUserBehaviour(){
+        database.deleteAll(Users)
+        driver = ChromeDriver()
+        driver.get("http://localhost:9999/")
+        val registerButton = driver.findElement(By.id("register-btn")).click()
+        val email: Unit = driver.findElement(By.id("emailinput")).sendKeys("mail@mail2.com")
+        val password: Unit = driver.findElement(By.id("passwordinput")).sendKeys("Password@123")
+        val username: Unit = driver.findElement(By.id("usernameinput")).sendKeys("Ahmed")
+        val submit = driver.findElement(By.id("submitbutton")).click()
+        Thread.sleep(2000)
+        val emailLogin: Unit = driver.findElement(By.id("email")).sendKeys("mail@mail2.com")
+        val passwordLogin: Unit = driver.findElement(By.id("password")).sendKeys("Password@123")
+        val submitLogin = driver.findElement(By.id("submit")).click()
+        val likeButton = driver.findElement(By.id("likeButton>Music")).click()
+        Thread.sleep(2000)
+        val clickOnProfileButton = driver.findElement(By.id("profile_picture")).click()
+        Thread.sleep(2000)
+        val likedAlbumElements = driver.findElements(By.className("album"))
+        val firstElementLiked = likedAlbumElements[0].text
+        assert(firstElementLiked.contains(">Music"))
+        Thread.sleep(2000)
+        val fileInput = driver.findElement(By.id("profilePictureInput"))
+        val filePath = dotenv["TEST_PHOTO"]
+        fileInput.sendKeys(filePath)
+        driver.findElement(By.id("photoForm")).submit()
+        val uploadedImage = driver.findElement(By.id("profilePicture"))
+        assert(uploadedImage.isDisplayed)
+        Thread.sleep(2000)
+
+
+        val logoutButton = driver.findElement(By.id("signout")).click()
+        val albumElements = driver.findElements(By.className("likedAlbumCard"))
+        val firstElement = albumElements[0].text
+        assert(firstElement.contains(">Music"))
+        Thread.sleep(1500)
+        driverShutDown()
+    }
+
+
+    fun setupAutoUser() {
+        database.deleteAll(Users)
         val chromeOptions = ChromeOptions()
         chromeOptions.addArguments("--headless")
         chromeOptions.addArguments("--disable-gpu")
         driver = ChromeDriver(chromeOptions)
-        driver.get("http://localhost:9999/users/new")
-    }
-
-    @BeforeEach
-    fun setupUser() {
-        //Register a new user
+        driver.get("http://localhost:9999/")
+        val registerButton = driver.findElement(By.id("register-btn")).click()
         val email: Unit = driver.findElement(By.id("emailinput")).sendKeys("mail@mail2.com")
-        Thread.sleep(2000)
         val password: Unit = driver.findElement(By.id("passwordinput")).sendKeys("Password@123")
-        Thread.sleep(2000)
-        val username: Unit = driver.findElement(By.id("usernameinput")).sendKeys("Ahmed")
-        Thread.sleep(2000)
+        val username: Unit = driver.findElement(By.id("usernameinput")).sendKeys("Sara")
         val submit = driver.findElement(By.id("submitbutton")).click()
-        Thread.sleep(2000)
-        //Login in the user created
         val emailLogin: Unit = driver.findElement(By.id("email")).sendKeys("mail@mail2.com")
-        Thread.sleep(2000)
         val passwordLogin: Unit = driver.findElement(By.id("password")).sendKeys("Password@123")
-        Thread.sleep(2000)
         val submitLogin = driver.findElement(By.id("submit")).click()
-        Thread.sleep(2000)
     }
 
-    @Test
-    fun `test clicking on the Like button`() {
-        val likeButton = driver.findElement(By.id("likeButton>Music")).click()
-        Thread.sleep(2000)
-        val albumElements = driver.findElements(By.className("album"))
-        val firstElement = albumElements[0].text
-        assert(firstElement.contains(">Music"))
-        Thread.sleep(2000)
-    }
-
-    @AfterAll
-    fun signOutUser(){
-        // Close the browser after the test is finished
-        val signOutButton = driver.findElement(By.id("signout")).click()
-        Thread.sleep(2000)
-    }
-    companion object {
-        @JvmStatic
-        @BeforeAll
-        fun restoreDb(): Unit {
-            database.delete(Users) { it.id eq it.id }
-        }
+    fun driverShutDown(){
+        driver.quit()
     }
 }
+
